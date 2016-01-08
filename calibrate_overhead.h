f63c9b7737c938e2cbd4c9cbef0d0915610f6008
Sterling McLeod <sterlingmcleod9@gmail.com>
2016-01-08 13:02:58 -0500

    Added code to calibrate the overhead Kinect

---
+++ calibrate_overhead.h
@@ -0,0 +1,23 @@
+#ifndef CALIBRATE_H
+#define CALIBRATE_H
+#include <iostream>
+#include <unistd.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/segmentation/region_growing_rgb.h>
+#include <pcl/search/search.h>
+#include <pcl/search/kdtree.h>
+#include <opencv2/opencv.hpp>
+#include "segment/segment-image.h"
+#include "segment/pnmfile.h"
+#include "tf/transform_datatypes.h"
+
+class Calibrate
+{
+};
+
+#endif
