8691c90a8eef44f61043065de79362980617cae8
sterlingm <sterlingmcleod9@gmail.com>
2018-01-19 11:44:57 -0500

    Re-organize the repo

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sterlingm/uncc_robo_lab
-# Build directory: /home/sterlingm/uncc_robo_lab/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
